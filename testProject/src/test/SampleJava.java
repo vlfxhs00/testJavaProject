fb9217236dc7efd5351a5850c7369fac73e3a3eb
mr2-19 <mr2-19@mr02-25>
2019-11-19 09:43:39 +0900

    소스를 수정해 봅니다.

--- testProject/src/test/SampleJava.java
+++ testProject/src/test/SampleJava.java
@@ -1,7 +1,8 @@
 package test;
 
 public class SampleJava {
-public static void main(String[] args) {
+	public static void main(String[] args) {
 	System.out.println("깃 허브 테스트용");
-}
+	System.out.println("두번째 테스트용 수정");
+	}
 }
